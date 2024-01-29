216ad4388bbf0ec21a6b7173fae96c0c57fca896
David van Buiten <david.vanbuiten@gmail.com>
2024-01-29 08:52:31 +0100

    Added the `Settings` class in `meldingen/config.py`. This class will read environment variable based configuration. It is based on the `pydantic-settings` package. Included a `dev.env` file with default development environment variables. Configured docker compose to use the `dev.env` file.

---
+++ meldingen/config.py
@@ -0,0 +1,19 @@
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """Settings class to manage configuration variables for the application."""
+    model_config = SettingsConfigDict(env_prefix="api_", env_nested_delimiter="__")
+
+    # General settings
+    debug: bool = False
+    project_name: str = "Meldingen Openbare Ruimte"
+    url_prefix: str = "/api"
+
+    # Database settings
+    database_dsn: PostgresDsn
+
+
+# Create an instance of the Settings model
+settings = Settings()
