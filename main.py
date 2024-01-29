4d5e632c201ed22231a924b6626600b293ef1033
Youri Westerman <y.westerman@amsterdam.nl>
2024-01-29 13:52:02 +0100

    Install dependency-injector dependency Unfortunately it does not support python 3.12 yet, so downgrading to 3.11 for now See: https://github.com/ets-labs/python-dependency-injector/pull/765
    Load settings in container
    
    Temporarily use fork of dependency-injector that provides python 3.12 support
    
    Switch back to python 3.12
    
    Run poetry update

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine
 
-from meldingen.config import settings
+from meldingen.config import Settings
+from meldingen.containers import Container
+
+container = Container()
+# TODO: We are currently unable to use `from_pydantic()`, because the `dependency-injector`
+# lacks support for pydantic v2.
+# https://python-dependency-injector.ets-labs.org/providers/configuration.html#loading-from-a-pydantic-settings
+container.settings.from_dict(Settings().model_dump())
 
 app = FastAPI(
-    debug=settings.debug,
-    title=settings.project_name,
-    prefix=settings.url_prefix,
+    debug=container.settings.get('debug'),
+    title=container.settings.get('project_name'),
+    prefix=container.settings.get('url_prefix'),
 )
 
-engine = create_engine(str(settings.database_dsn), echo=True)
+engine = create_engine(str(container.settings.get('database_dsn')), echo=True)
