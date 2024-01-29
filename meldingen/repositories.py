1dab04fd64caa65d4097a54326bcee4143af6b30
Youri Westerman <y.westerman@amsterdam.nl>
2024-01-29 12:49:41 +0100

    Add repository for melding

---
+++ meldingen/repositories.py
@@ -0,0 +1,17 @@
+from meldingen_core.models import Melding
+from meldingen_core.repositories import BaseMeldingRepository
+from sqlmodel import Session
+
+
+class BaseSQLModelRepository:
+    """Base repository for SQLModel based repositories."""
+    _session: Session
+
+    def __init__(self, session: Session) -> None:
+        self._session = session
+
+
+class MeldingRepository(BaseSQLModelRepository, BaseMeldingRepository):
+    """Repository for Melding model."""
+    def add(self, melding: Melding) -> None:
+        self._session.add(melding)
