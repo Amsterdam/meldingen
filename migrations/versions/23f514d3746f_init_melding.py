d07277ce853eda54c71fc0806322befdf55087cd
David van Buiten <david.vanbuiten@gmail.com>
2024-01-29 16:15:00 +0100

    Fixing iSort issues

--- migrations/versions/23f514d3746f_init_melding.py
+++ migrations/versions/23f514d3746f_init_melding.py
@@ -8,10 +8,9 @@ Create Date: 2024-01-29 12:29:58.729280
 
 from typing import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 import sqlmodel
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "23f514d3746f"
