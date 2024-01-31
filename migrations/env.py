ae47ac22cab999c748b3e734bf4a2ea7c0180c0f
Youri Westerman <y.westerman@amsterdam.nl>
2024-01-31 11:49:53 +0100

    Revert "Remove unused import"
    This reverts commit 5d25104fac7099f93125324cfe32026150daf09d.

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
 from meldingen.config import settings
+from meldingen.models import Melding
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
