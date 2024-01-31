b7d47d61af731bc4762e35d137b48bbdab7a35ea
Youri Westerman <y.westerman@amsterdam.nl>
2024-01-31 15:16:05 +0100

    Create application and client for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
+from typing import AsyncGenerator, Generator
+
 import alembic
 import pytest
 import sqlalchemy
 from alembic.config import Config
+from asgi_lifespan import LifespanManager
+from fastapi import FastAPI
+from httpx import AsyncClient
 from pytest_alembic.config import Config as PytestAlembicConfig
 from sqlalchemy import Engine
 
@@ -19,8 +24,25 @@ def alembic_engine() -> Engine:
 
 
 @pytest.fixture
-def apply_migrations() -> None:
+def apply_migrations() -> Generator[None, None, None]:
     config = Config("alembic.ini")
 
-    alembic.command.downgrade(config, "base")
     alembic.command.upgrade(config, "head")
+    yield
+    alembic.command.downgrade(config, "base")
+
+
+@pytest.fixture
+def app(apply_migrations: None) -> FastAPI:
+    from main import get_application
+
+    return get_application()
+
+
+@pytest.fixture
+async def client(app: FastAPI) -> AsyncGenerator[AsyncClient, None]:
+    async with LifespanManager(app):
+        async with AsyncClient(
+            app=app, base_url="http://testserver", headers={"Content-Type": "application/json"}
+        ) as client:
+            yield client
