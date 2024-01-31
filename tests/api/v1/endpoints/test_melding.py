72798f2fadf7e8426d3ed03ce574fb21a8056e48
Youri Westerman <y.westerman@amsterdam.nl>
2024-01-31 16:17:15 +0100

    Add route tests

--- tests/api/v1/endpoints/test_melding.py
+++ tests/api/v1/endpoints/test_melding.py
@@ -1,14 +1,29 @@
-from fastapi.testclient import TestClient
+from typing import Final
 
-from main import app
+import pytest
+from fastapi import FastAPI
+from httpx import AsyncClient
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_422_UNPROCESSABLE_ENTITY, HTTP_200_OK
+
+ROUTE_NAME_CREATE: Final[str] = "melding:create"
+
+class TestMeldingRoutes:
+    @pytest.mark.asyncio
+    async def test_routes_exist(self, app: FastAPI, client: AsyncClient) -> None:
+        res = await client.post(app.url_path_for(ROUTE_NAME_CREATE), json={})
+        assert res.status_code != HTTP_404_NOT_FOUND
+
+    @pytest.mark.asyncio
+    async def test_invalid_input_raises_error(self, app: FastAPI, client: AsyncClient) -> None:
+        res = await client.post(app.url_path_for(ROUTE_NAME_CREATE), json={})
+        assert res.status_code == HTTP_422_UNPROCESSABLE_ENTITY
 
 
-def test_create_melding(apply_migrations: None) -> None:
-    client = TestClient(app)
+@pytest.mark.asyncio
+async def test_create_melding(app: FastAPI, client: AsyncClient) -> None:
+    response = await client.post(app.url_path_for(ROUTE_NAME_CREATE), json={"text": "This is a test melding."})
 
-    response = client.post(app.url_path_for("melding:create"), json={"text": "This is a test melding."})
-
-    assert response.status_code == 200
+    assert response.status_code == HTTP_200_OK
 
     data = response.json()
     assert data.get("id") == 1
