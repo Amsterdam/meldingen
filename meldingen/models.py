8d7d582766661751d2b87bb5426400fe87f0cfcf
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-06 11:53:25 +0100

    Add user model

--- meldingen/models.py
+++ meldingen/models.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from meldingen_core.models import Melding as BaseMelding
 from sqlmodel import Field, SQLModel
+from starlette.authentication import SimpleUser
 
 
 class MeldingCreateInput(BaseMelding, SQLModel): ...
@@ -11,3 +12,9 @@ class Melding(BaseMelding, SQLModel, table=True):
     """SQLModel for Melding."""
 
     id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class User(SimpleUser, SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(min_length=1, max_length=254, unique=True)
+    email: str = Field(min_length=5, max_length=254, unique=True)
