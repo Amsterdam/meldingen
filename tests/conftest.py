f338df75f865f974635d1c15f534bc32eff83182
David van Buiten <david.vanbuiten+github@gmail.com>
2024-02-09 09:40:31 +0100

    When running tests the test database must be used (#19)
    * When running tests the test database must be used
    
    * Make sure that the testdatabase is created correctly and all tests run

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,18 @@
 from typing import AsyncGenerator, Generator
 
-import alembic
 import pytest
 import pytest_asyncio
 import sqlalchemy
-from alembic.config import Config
 from asgi_lifespan import LifespanManager
+from dependency_injector import containers
+from dependency_injector.providers import Object, Resource
 from fastapi import FastAPI
 from httpx import AsyncClient
 from pytest_alembic.config import Config as PytestAlembicConfig
 from sqlalchemy import Engine
+from sqlmodel import Session
+
+TEST_DATABASE_URL: str = "postgresql://meldingen:postgres@database:5432/meldingen-test"
 
 
 @pytest.fixture
@@ -18,23 +21,34 @@ def alembic_config() -> PytestAlembicConfig:
     return PytestAlembicConfig()
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def alembic_engine() -> Engine:
     """Override this fixture to provide pytest-alembic powered tests with a database handle."""
-    return sqlalchemy.create_engine("postgresql://meldingen:postgres@database:5432/meldingen-test")
+    return sqlalchemy.create_engine(TEST_DATABASE_URL, isolation_level="AUTOCOMMIT", echo=True)
 
 
-@pytest_asyncio.fixture(scope="session")
-def apply_migrations() -> None:
-    config = Config("alembic.ini")
+@pytest.fixture(scope="session")
+def test_database(alembic_engine: Engine) -> None:
+    from sqlmodel import SQLModel
 
-    alembic.command.downgrade(config, "base")
-    alembic.command.upgrade(config, "head")
+    SQLModel.metadata.drop_all(alembic_engine)
+    SQLModel.metadata.create_all(alembic_engine)
 
 
 @pytest_asyncio.fixture
-def app(apply_migrations: None) -> FastAPI:
+def app(test_database: None, alembic_engine: Engine) -> FastAPI:
     from main import get_application
+    from meldingen.containers import Container
+
+    def get_database_session(engine: Engine) -> Generator[Session, None, None]:
+        session = Session(engine)
+        yield session
+        session.close()
+
+    @containers.override(Container)
+    class TestContainer(containers.DeclarativeContainer):
+        database_engine: Object[Engine] = Object(alembic_engine)
+        database_session: Resource[Session] = Resource(get_database_session, engine=database_engine)
 
     return get_application()
 
