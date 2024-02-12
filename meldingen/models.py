e41a8d2a3c0b61fafa313340e63876800c9b5613
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-12 14:18:15 +0100

    Remove dependency on starlette's SimpleUser

--- meldingen/models.py
+++ meldingen/models.py
@@ -2,7 +2,6 @@ from typing import Optional
 
 from meldingen_core.models import Melding as BaseMelding
 from sqlmodel import Field, SQLModel
-from starlette.authentication import SimpleUser
 
 
 class MeldingCreateInput(BaseMelding, SQLModel): ...
@@ -14,7 +13,7 @@ class Melding(BaseMelding, SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
 
 
-class User(SimpleUser, SQLModel, table=True):
+class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(min_length=1, max_length=254, unique=True)
     email: str = Field(min_length=5, max_length=254, unique=True)
