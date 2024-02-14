bb7015a4fb1bff74a6b25c7236042f609ae70770
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-14 17:05:00 +0100

    Add group and association model

--- meldingen/models.py
+++ meldingen/models.py
@@ -3,7 +3,7 @@ from typing import Optional
 from meldingen_core.models import Melding as BaseMelding
 from meldingen_core.models import User as BaseUser
 from pydantic import EmailStr
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class BaseDBModel(SQLModel):
@@ -21,6 +21,17 @@ class UserCreateInput(BaseUser, SQLModel):
     email: EmailStr
 
 
+class UserGroup(SQLModel, table=True):
+    user_id: int | None = Field(default=None, foreign_key="user.id", primary_key=True)
+    group_id: int | None = Field(default=None, foreign_key="group.id", primary_key=True)
+
+
 class User(BaseUser, BaseDBModel, table=True):
     username: str = Field(min_length=1, max_length=320, unique=True)
     email: str = Field(min_length=5, max_length=320, unique=True)
+    groups: list["Group"] = Relationship(back_populates="users", link_model=UserGroup, sa_relationship_kwargs={"lazy": "joined"})
+
+
+class Group(BaseDBModel, table=True):
+    name: str = Field(min_length=1, unique=True)
+    users: list[User] = Relationship(back_populates="groups", link_model=UserGroup)
