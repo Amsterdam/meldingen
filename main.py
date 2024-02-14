2740843c6b3ac2e95d129996e8a74be17d0e0771
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-14 17:07:17 +0100

    Add commands to add group and to assign user to group

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import typer
 
 from meldingen.config import Settings
 from meldingen.containers import Container
-from meldingen.models import User, UserCreateInput
+from meldingen.models import Group, User, UserCreateInput
 
 app = typer.Typer()
 container = Container()
@@ -23,6 +23,33 @@ async def async_add_user(email: str) -> None:
 @app.command()
 def add_user(email: str) -> None:
     asyncio.run(async_add_user(email))
+
+
+async def async_add_group(name: str) -> None:
+    group_repository = await container.group_repository()
+
+    await group_repository.save(Group(name=name))
+
+@app.command()
+def add_group(name: str) -> None:
+    asyncio.run(async_add_group(name))
+
+
+async def async_add_user_to_group(email: str, group_name: str) -> None:
+    user_repository = await container.user_repository()
+    group_repository = await container.group_repository()
+
+    user = await user_repository.find_by_email(email)
+    group = await group_repository.find_by_name(group_name)
+
+    user.groups.append(group)
+
+    await user_repository.save(user)
+
+
+@app.command()
+def add_user_to_group(email: str, group_name: str) -> None:
+    asyncio.run(async_add_user_to_group(email, group_name))
 
 
 if __name__ == "__main__":
