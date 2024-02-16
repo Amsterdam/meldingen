daf4e49c58ca91f55046b1675f8ae7ae059dc154
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-16 16:40:41 +0100

    Create users, groups and rules

--- meldingen-entrypoint.sh
+++ meldingen-entrypoint.sh
@@ -4,4 +4,12 @@ set -eux
 # Run Alembic migrations
 alembic upgrade head
 
+# Create users and groups
+python main.py add-user user@example.com
+python main.py add-user admin@example.com
+
+python main.py add-group admins
+
+python main.py add-casbin-rules
+
 exec "$@"
