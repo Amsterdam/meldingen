8fdb7e0140e322cd8f71341e27a30a17bcd28e66
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-20 14:25:42 +0100

    Import models instead of a single model

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlmodel import SQLModel
 
 from meldingen.config import settings
-from meldingen.models import Melding
+from meldingen import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
