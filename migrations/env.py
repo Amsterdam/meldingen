d574229cf77e93fcc2f38fffc8f29131d8a469ea
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-21 16:31:32 +0100

    Run isort

--- migrations/env.py
+++ migrations/env.py
@@ -6,8 +6,8 @@ from sqlalchemy import Connection, pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlmodel import SQLModel
 
-from meldingen.config import settings
 from meldingen import models
+from meldingen.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
