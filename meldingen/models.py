6b3b60775ffbbde52e39bc4130f8ae2d5ae099a5
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-23 12:38:59 +0100

    Improve models

--- meldingen/models.py
+++ meldingen/models.py
@@ -3,13 +3,13 @@ from typing import Optional
 from meldingen_core.models import Classification as BaseClassification
 from meldingen_core.models import Melding as BaseMelding
 from meldingen_core.models import User as BaseUser
-from pydantic import EmailStr, BaseModel
-from sqlalchemy import String, Table, Integer, Column, ForeignKey
-from sqlalchemy.orm import MappedAsDataclass, DeclarativeBase, Mapped, mapped_column, declared_attr, relationship
+from pydantic import BaseModel, EmailStr
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import DeclarativeBase, Mapped, MappedAsDataclass, declared_attr, mapped_column, relationship
 
 
 class BaseDBModel(MappedAsDataclass, DeclarativeBase):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
@@ -25,8 +25,8 @@ class MeldingCreateInput(BaseModel):
 
 class Melding(BaseDBModel, BaseMelding):
     text: Mapped[str] = mapped_column(String)
-    classification_id: Mapped[int] = mapped_column(ForeignKey("classification.id"), nullable=True)
-    classification: Mapped[Classification | None] = relationship()
+    classification_id: Mapped[int | None] = mapped_column(ForeignKey("classification.id"), default=None)
+    classification: Mapped[Classification | None] = relationship(default=None)
 
 
 class UserInput(BaseUser, BaseModel):
@@ -49,7 +49,9 @@ user_group = Table(
 class User(BaseDBModel, BaseUser):
     username: Mapped[str] = mapped_column(String(320), unique=True)
     email: Mapped[str] = mapped_column(String(320), unique=True)
-    groups: Mapped[list["Group"]] = relationship(secondary=user_group, back_populates="users", lazy="joined")
+    groups: Mapped[list["Group"]] = relationship(
+        secondary=user_group, back_populates="users", lazy="joined", default_factory=list
+    )
 
 
 class Group(BaseDBModel):
