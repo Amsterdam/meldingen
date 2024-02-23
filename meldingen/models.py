f96da5fda40559eea0d88e7879edc4b4aad7e6d9
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-23 12:13:13 +0100

    Convert models to SQLAlchemy ORM models

--- meldingen/models.py
+++ meldingen/models.py
@@ -3,50 +3,55 @@ from typing import Optional
 from meldingen_core.models import Classification as BaseClassification
 from meldingen_core.models import Melding as BaseMelding
 from meldingen_core.models import User as BaseUser
-from pydantic import EmailStr
-from sqlmodel import Field, Relationship, SQLModel
+from pydantic import EmailStr, BaseModel
+from sqlalchemy import String, Table, Integer, Column, ForeignKey
+from sqlalchemy.orm import MappedAsDataclass, DeclarativeBase, Mapped, mapped_column, declared_attr, relationship
 
 
-class BaseDBModel(SQLModel):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class BaseDBModel(MappedAsDataclass, DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
 
 
-class Classification(BaseClassification, BaseDBModel, table=True): ...
+class Classification(BaseDBModel, BaseClassification): ...
 
 
-class MeldingCreateInput(SQLModel):
+class MeldingCreateInput(BaseModel):
     text: str
 
 
-class Melding(BaseMelding, BaseDBModel, table=True):
-    """SQLModel for Melding."""
+class Melding(BaseDBModel, BaseMelding):
+    text: Mapped[str] = mapped_column(String)
+    classification_id: Mapped[int] = mapped_column(ForeignKey("classification.id"), nullable=True)
+    classification: Mapped[Classification | None] = relationship()
 
-    classification_id: int | None = Field(default=None, foreign_key="classification.id")
-    classification: Classification = Relationship()
 
-
-class UserInput(BaseUser, SQLModel):
+class UserInput(BaseUser, BaseModel):
     email: EmailStr
 
 
-class UserPartialInput(SQLModel):
+class UserPartialInput(BaseModel):
     username: Optional[str] = None
     email: Optional[EmailStr] = None
 
 
-class UserGroup(SQLModel, table=True):
-    user_id: int | None = Field(default=None, foreign_key="user.id", primary_key=True)
-    group_id: int | None = Field(default=None, foreign_key="group.id", primary_key=True)
+user_group = Table(
+    "user_group",
+    BaseDBModel.metadata,
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key=True),
+    Column("group_id", Integer, ForeignKey("group.id"), primary_key=True),
+)
 
 
-class User(BaseUser, BaseDBModel, table=True):
-    username: str = Field(min_length=1, max_length=320, unique=True)
-    email: str = Field(min_length=5, max_length=320, unique=True)
-    groups: list["Group"] = Relationship(
-        back_populates="users", link_model=UserGroup, sa_relationship_kwargs={"lazy": "joined"}
-    )
+class User(BaseDBModel, BaseUser):
+    username: Mapped[str] = mapped_column(String(320), unique=True)
+    email: Mapped[str] = mapped_column(String(320), unique=True)
+    groups: Mapped[list["Group"]] = relationship(secondary=user_group, back_populates="users", lazy="joined")
 
 
-class Group(BaseDBModel, table=True):
-    name: str = Field(min_length=1, unique=True)
-    users: list[User] = Relationship(back_populates="groups", link_model=UserGroup)
+class Group(BaseDBModel):
+    name: Mapped[str] = mapped_column(unique=True)
+    users: Mapped[list[User]] = relationship(secondary=user_group, back_populates="groups")
