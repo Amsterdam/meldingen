0201ae95d1f66f352c59c4272f7d4a7486db45ea
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-28 12:02:48 +0100

    Override core actions to support our derived classification model

--- meldingen/actions.py
+++ meldingen/actions.py
@@ -1,9 +1,11 @@
+from meldingen_core.actions.classification import ClassificationListAction as BaseClassificationListAction
+from meldingen_core.actions.classification import ClassificationRetrieveAction as BaseClassificationRetrieveAction
 from meldingen_core.actions.melding import MeldingListAction as BaseMeldingListAction
 from meldingen_core.actions.melding import MeldingRetrieveAction as BaseMeldingRetrieveAction
 from meldingen_core.actions.user import UserListAction as BaseUserListAction
 from meldingen_core.actions.user import UserRetrieveAction as BaseUserRetrieveAction
 
-from meldingen.models import Melding, User
+from meldingen.models import Classification, Melding, User
 
 
 class UserListAction(BaseUserListAction[User, User]): ...
@@ -16,3 +18,9 @@ class MeldingListAction(BaseMeldingListAction[Melding, Melding]): ...
 
 
 class MeldingRetrieveAction(BaseMeldingRetrieveAction[Melding, Melding]): ...
+
+
+class ClassificationListAction(BaseClassificationListAction[Classification, Classification]): ...
+
+
+class ClassificationRetrieveAction(BaseClassificationRetrieveAction[Classification, Classification]): ...
