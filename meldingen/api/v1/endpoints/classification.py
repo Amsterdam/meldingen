7a98c991766d7f5d4105730cfbf34fd7f8e23dc4
Youri Westerman <y.westerman@amsterdam.nl>
2024-02-28 12:04:32 +0100

    Implement path operation for the classification list endpoint

--- meldingen/api/v1/endpoints/classification.py
+++ meldingen/api/v1/endpoints/classification.py
@@ -1,15 +1,17 @@
-from dependency_injector.wiring import inject, Provide
+from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
-from meldingen.containers import Container
 from meldingen_core.actions.classification import ClassificationCreateAction
 
+from meldingen.actions import ClassificationListAction
+from meldingen.api.utils import pagination_params
 from meldingen.authentication import authenticate_user
+from meldingen.containers import Container
 from meldingen.models import Classification, ClassificationInput, ClassificationOutput, User
 
 router = APIRouter()
 
 
-@router.get("/", name="classification:create", status_code=201)
+@router.post("/", name="classification:create", status_code=201)
 @inject
 async def create_classification(
     classification_input: ClassificationInput,
@@ -21,3 +23,22 @@ async def create_classification(
     await action(db_model)
 
     return ClassificationOutput(id=db_model.id, name=db_model.name)
+
+
+@router.get("/", name="classification:list")
+@inject
+async def list_classifications(
+    pagination: dict[str, int | None] = Depends(pagination_params),
+    action: ClassificationListAction = Depends(Provide[Container.classification_list_action]),
+    user: User = Depends(authenticate_user),
+) -> list[ClassificationOutput]:
+    limit = pagination["limit"] or 0
+    offset = pagination["offset"] or 0
+
+    classifications = await action(limit=limit, offset=offset)
+
+    output = []
+    for classification in classifications:
+        output.append(ClassificationOutput(id=classification.id, name=classification.name))
+
+    return output
