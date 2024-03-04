6a235cb8f9ef1b0840aa197f20038e1f5dcee56b
David van Buiten <david.vanbuiten+github@gmail.com>
2024-03-04 10:58:55 +0100

    Integrate the `asgi-correlation-id` package to auto-assign request_id on each request and ensuring its presence in the response headers. Refactor structlog to use this request_id instead of generating its own. (#43)

--- meldingen/__init__.py
+++ meldingen/__init__.py
@@ -2,6 +2,8 @@ import logging
 import sys
 
 import structlog
+from asgi_correlation_id import correlation_id
+from structlog.typing import EventDict, WrappedLogger
 
 from meldingen.config import settings
 
@@ -18,10 +20,19 @@ log_level = logging.DEBUG if settings.debug else settings.log_level
 # Standard logging configuration
 logging.basicConfig(format="%(message)s", stream=sys.stdout, level=log_level)
 
+
+def add_correlation(logger: WrappedLogger, method_name: str, event_dict: EventDict) -> EventDict:
+    """Add the request id to the EventDict."""
+    if request_id := correlation_id.get():
+        event_dict["request_id"] = request_id
+    return event_dict
+
+
 # Structlog configuration
 structlog.configure(
     wrapper_class=structlog.make_filtering_bound_logger(log_level),
     processors=[
+        add_correlation,
         structlog.contextvars.merge_contextvars,
         structlog.processors.add_log_level,
         structlog.processors.StackInfoRenderer(),
