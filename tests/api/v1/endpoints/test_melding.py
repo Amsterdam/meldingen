8662b11af872f31bd3e1ed00e76aeedbbeebb50a
Youri Westerman <y.westerman@amsterdam.nl>
2024-03-13 12:04:07 +0100

    Add tests for complete transition endpoint

--- tests/api/v1/endpoints/test_melding.py
+++ tests/api/v1/endpoints/test_melding.py
@@ -175,3 +175,59 @@ class TestMeldingProcess(BaseUnauthorizedTest):
         body = response.json()
 
         assert body.get("detail") == "Transition not allowed from current state"
+
+
+class TestMeldingComplete(BaseUnauthorizedTest):
+    def get_route_name(self) -> str:
+        return "melding:complete"
+
+    def get_method(self) -> str:
+        return "PUT"
+
+    def get_path_params(self) -> dict[str, Any]:
+        return {"melding_id": 1}
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(["melding_text", "melding_state"], [("Er ligt poep op de stoep.", MeldingStates.PROCESSING)], indirect=True)
+    async def test_complete_melding(
+        self, app: FastAPI, client: AsyncClient, auth_user: None, test_melding: Melding
+    ) -> None:
+        response = await client.request(
+            self.get_method(), app.url_path_for(self.get_route_name(), **self.get_path_params())
+        )
+
+        assert response.status_code == HTTP_200_OK
+
+        body = response.json()
+
+        assert body.get("state") == MeldingStates.COMPLETED
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("melding_text", ["Er ligt poep op de stoep."], indirect=True)
+    async def test_complete_melding_not_found(
+        self, app: FastAPI, client: AsyncClient, auth_user: None, test_melding: Melding
+    ) -> None:
+        response = await client.request(self.get_method(), app.url_path_for(self.get_route_name(), melding_id=404))
+
+        assert response.status_code == HTTP_404_NOT_FOUND
+
+        body = response.json()
+
+        assert body.get("detail") == "Not Found"
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        ["melding_text", "melding_state"], [("Er ligt poep op de stoep.", MeldingStates.COMPLETED)], indirect=True
+    )
+    async def test_complete_melding_wrong_state(
+        self, app: FastAPI, client: AsyncClient, auth_user: None, test_melding: Melding
+    ) -> None:
+        response = await client.request(
+            self.get_method(), app.url_path_for(self.get_route_name(), **self.get_path_params())
+        )
+
+        assert response.status_code == HTTP_400_BAD_REQUEST
+
+        body = response.json()
+
+        assert body.get("detail") == "Transition not allowed from current state"
