77b980f49610a5da19852e2ea4b52f0fb266d6e1
David van Buiten <david.vanbuiten@gmail.com>
2024-03-14 16:05:39 +0100

    Added CLI command for primary form creation, ensuring only 1 primary form exists. The primary form is created component-free, this command is used for basic setup.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import typer
 
-from commands import groups, users
+from commands import forms, groups, users
 
 app = typer.Typer()
 app.add_typer(users.app, name="users")
 app.add_typer(groups.app, name="groups")
+app.add_typer(forms.app, name="forms")
 
 
 if __name__ == "__main__":
