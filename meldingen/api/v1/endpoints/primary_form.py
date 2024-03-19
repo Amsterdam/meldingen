84856dd3bee406fbeb5898c6d16688e7c0d61fd9
David van Buiten <david.vanbuiten+github@gmail.com>
2024-03-19 10:29:11 +0100

    Added the endpoint to update the primary form with associated action and tests (#62)

--- meldingen/api/v1/endpoints/primary_form.py
+++ meldingen/api/v1/endpoints/primary_form.py
@@ -1,17 +1,21 @@
+from typing import Annotated
+
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends, HTTPException
+from meldingen_core.exceptions import NotFoundException
 from starlette.status import HTTP_404_NOT_FOUND
 
-from meldingen.actions import FormIoPrimaryFormRetrieveAction
-from meldingen.api.v1 import not_found_response
+from meldingen.actions import FormIoPrimaryFormRetrieveAction, FormIoPrimaryFormUpdateAction
+from meldingen.api.v1 import not_found_response, unauthorized_response
+from meldingen.authentication import authenticate_user
 from meldingen.containers import Container
-from meldingen.models import FormIoForm
-from meldingen.schemas import FormComponentOutput, FormOutput
+from meldingen.models import FormIoForm, User
+from meldingen.schemas import FormComponentOutput, FormOutput, FormUpdateInput
 
 router = APIRouter()
 
 
-async def _form_output_schema(form: FormIoForm) -> FormOutput:
+async def _hydrate_output(form: FormIoForm) -> FormOutput:
     components_output = [
         FormComponentOutput(
             label=component.label,
@@ -40,4 +44,22 @@ async def retrieve_primary_form(
     if not db_form:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND)
 
-    return await _form_output_schema(form=db_form)
+    return await _hydrate_output(form=db_form)
+
+
+@router.put("/", name="primary-form:update", responses={**unauthorized_response, **not_found_response})
+@inject
+async def update_primary_form(
+    form_input: FormUpdateInput,
+    user: Annotated[User, Depends(authenticate_user)],
+    action: FormIoPrimaryFormUpdateAction = Depends(Provide(Container.primary_form_update_action)),
+) -> FormOutput:
+    """Update the primary form that is used to initiate the creation of a "Melding"."""
+    form_data = form_input.model_dump(exclude_unset=True)
+
+    try:
+        db_form = await action(form_data)
+    except NotFoundException:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+
+    return await _hydrate_output(form=db_form)
