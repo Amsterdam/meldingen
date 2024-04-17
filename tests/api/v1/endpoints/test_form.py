d02d1724fc7299bb095fe071102218de680dd95f
Youri Westerman <y.westerman@amsterdam.nl>
2024-04-17 15:17:33 +0200

    Add test to verify that the validation of the classification id works correctly when creating form

--- tests/api/v1/endpoints/test_form.py
+++ tests/api/v1/endpoints/test_form.py
@@ -392,3 +392,21 @@ class TestFormCreate(BaseUnauthorizedTest):
         assert violation.get("type") == error_type
         assert violation.get("loc") == error_loc
         assert violation.get("msg") == error_msg
+
+    @pytest.mark.asyncio
+    async def test_validation_of_classification_id(self, app: FastAPI, client: AsyncClient, auth_user: None) -> None:
+        response = await client.post(
+            app.url_path_for(self.ROUTE_NAME),
+            json={"title": "title", "display": "form", "classification": 0, "components": []},
+        )
+
+        assert response.status_code == HTTP_422_UNPROCESSABLE_ENTITY
+
+        body = response.json()
+        detail = body.get("detail")
+        assert len(detail) == 1
+
+        violation = detail[0]
+        assert violation.get("type") == "greater_than"
+        assert violation.get("loc") == ["body", "classification"]
+        assert violation.get("msg") == "Input should be greater than 0"
