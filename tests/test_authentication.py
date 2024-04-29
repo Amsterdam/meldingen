e9e84f2d1bca7b5910d334bebc913e28e6225a4a
Youri Westerman <y.westerman@amsterdam.nl>
2024-04-29 17:27:20 +0200

    Add test to confirm invalid token behavior

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -11,5 +11,17 @@ from meldingen.authentication import UnauthenticatedException, get_user
 async def test_get_user_token_expired(app: FastAPI, py_jwt_mock: Mock) -> None:
     py_jwt_mock.decode.side_effect = ExpiredSignatureError()
 
-    with pytest.raises(UnauthenticatedException):
+    with pytest.raises(UnauthenticatedException) as exc_info:
         await get_user("123456789")
+
+    assert exc_info.value.detail == "Token has expired"
+
+
+@pytest.mark.asyncio
+async def test_get_user_token_invalid(app: FastAPI, py_jwt_mock: Mock) -> None:
+    py_jwt_mock.decode.return_value = {}
+
+    with pytest.raises(UnauthenticatedException) as exc_info:
+        await get_user("123456789")
+
+    assert exc_info.value.detail == "Invalid token"
