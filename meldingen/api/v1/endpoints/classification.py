20844df26796cb09963658c840e2c7da9e81688b
Youri Westerman <y.westerman@amsterdam.nl>
2024-05-01 11:47:02 +0200

    Add content-range header to classification list endpoint

--- meldingen/api/v1/endpoints/classification.py
+++ meldingen/api/v1/endpoints/classification.py
@@ -1,17 +1,18 @@
 from typing import Annotated
 
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException, Path, Response
 from meldingen_core.actions.classification import ClassificationCreateAction, ClassificationDeleteAction
 from meldingen_core.exceptions import NotFoundException
 from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND
 
 from meldingen.actions import ClassificationListAction, ClassificationRetrieveAction, ClassificationUpdateAction
 from meldingen.api.utils import PaginationParams, pagination_params
-from meldingen.api.v1 import conflict_response, not_found_response, unauthorized_response
+from meldingen.api.v1 import conflict_response, list_response, not_found_response, unauthorized_response
 from meldingen.authentication import authenticate_user
 from meldingen.containers import Container
 from meldingen.models import Classification, User
+from meldingen.repositories import ClassificationRepository
 from meldingen.schemas import ClassificationInput, ClassificationOutput
 
 router = APIRouter()
@@ -36,12 +37,14 @@ async def create_classification(
     return ClassificationOutput(id=db_model.id, name=db_model.name)
 
 
-@router.get("/", name="classification:list", responses={**unauthorized_response})
+@router.get("/", name="classification:list", responses={**list_response, **unauthorized_response})
 @inject
 async def list_classifications(
+    response: Response,
     pagination: Annotated[PaginationParams, Depends(pagination_params)],
     user: Annotated[User, Depends(authenticate_user)],
     action: ClassificationListAction = Depends(Provide[Container.classification_list_action]),
+    repository: ClassificationRepository = Depends(Provide[Container.classification_repository]),
 ) -> list[ClassificationOutput]:
     limit = pagination["limit"] or 0
     offset = pagination["offset"] or 0
@@ -51,6 +54,8 @@ async def list_classifications(
     output = []
     for classification in classifications:
         output.append(ClassificationOutput(id=classification.id, name=classification.name))
+
+    response.headers["Content-Range"] = f"classification {offset}-{limit - 1 + offset}/{await repository.count()}"
 
     return output
 
