a50535c5dd793f534c68691963341893e5dec3ac
Youri Westerman <y.westerman@amsterdam.nl>
2024-06-21 11:31:56 +0200

    Disable typer command loading

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import typer
-
-from commands import groups, static_forms, users
-
+#
+# from commands import groups, static_forms, users
+#
 app = typer.Typer()
-app.add_typer(users.app, name="users")
-app.add_typer(groups.app, name="groups")
-app.add_typer(static_forms.app, name="static-forms")
-
-
+# app.add_typer(users.app, name="users")
+# app.add_typer(groups.app, name="groups")
+# app.add_typer(static_forms.app, name="static-forms")
+#
+#
 if __name__ == "__main__":
     app()
