add5c663c4b7761fe3b8ce1fef587f58d649963f
Youri Westerman <y.westerman@amsterdam.nl>
2024-06-25 14:32:36 +0200

    Re-enable test_list_classifications_sorted

--- tests/api/v1/endpoints/test_classification.py
+++ tests/api/v1/endpoints/test_classification.py
@@ -121,103 +121,103 @@ class TestClassificationList(BaseUnauthorizedTest, BasePaginationParamsTest, Bas
 
         assert response.headers.get("content-range") == f"classification {offset}-{limit - 1 + offset}/10"
 
-#     @pytest.mark.asyncio
-#     @pytest.mark.parametrize(
-#         "attribute, direction, expected",
-#         [
-#             (
-#                 "id",
-#                 SortingDirection.ASC,
-#                 [
-#                     {"name": "category: 0", "id": 1, "form": None},
-#                     {"name": "category: 1", "id": 2, "form": None},
-#                     {"name": "category: 2", "id": 3, "form": None},
-#                     {"name": "category: 3", "id": 4, "form": None},
-#                     {"name": "category: 4", "id": 5, "form": None},
-#                     {"name": "category: 5", "id": 6, "form": None},
-#                     {"name": "category: 6", "id": 7, "form": None},
-#                     {"name": "category: 7", "id": 8, "form": None},
-#                     {"name": "category: 8", "id": 9, "form": None},
-#                     {"name": "category: 9", "id": 10, "form": None},
-#                 ],
-#             ),
-#             (
-#                 "id",
-#                 SortingDirection.DESC,
-#                 [
-#                     {"name": "category: 9", "id": 10, "form": None},
-#                     {"name": "category: 8", "id": 9, "form": None},
-#                     {"name": "category: 7", "id": 8, "form": None},
-#                     {"name": "category: 6", "id": 7, "form": None},
-#                     {"name": "category: 5", "id": 6, "form": None},
-#                     {"name": "category: 4", "id": 5, "form": None},
-#                     {"name": "category: 3", "id": 4, "form": None},
-#                     {"name": "category: 2", "id": 3, "form": None},
-#                     {"name": "category: 1", "id": 2, "form": None},
-#                     {"name": "category: 0", "id": 1, "form": None},
-#                 ],
-#             ),
-#             (
-#                 "name",
-#                 SortingDirection.ASC,
-#                 [
-#                     {"name": "category: 0", "id": 1, "form": None},
-#                     {"name": "category: 1", "id": 2, "form": None},
-#                     {"name": "category: 2", "id": 3, "form": None},
-#                     {"name": "category: 3", "id": 4, "form": None},
-#                     {"name": "category: 4", "id": 5, "form": None},
-#                     {"name": "category: 5", "id": 6, "form": None},
-#                     {"name": "category: 6", "id": 7, "form": None},
-#                     {"name": "category: 7", "id": 8, "form": None},
-#                     {"name": "category: 8", "id": 9, "form": None},
-#                     {"name": "category: 9", "id": 10, "form": None},
-#                 ],
-#             ),
-#             (
-#                 "name",
-#                 SortingDirection.DESC,
-#                 [
-#                     {"name": "category: 9", "id": 10, "form": None},
-#                     {"name": "category: 8", "id": 9, "form": None},
-#                     {"name": "category: 7", "id": 8, "form": None},
-#                     {"name": "category: 6", "id": 7, "form": None},
-#                     {"name": "category: 5", "id": 6, "form": None},
-#                     {"name": "category: 4", "id": 5, "form": None},
-#                     {"name": "category: 3", "id": 4, "form": None},
-#                     {"name": "category: 2", "id": 3, "form": None},
-#                     {"name": "category: 1", "id": 2, "form": None},
-#                     {"name": "category: 0", "id": 1, "form": None},
-#                 ],
-#             ),
-#         ],
-#     )
-#     async def test_list_classifications_sorted(
-#         self,
-#         app: FastAPI,
-#         client: AsyncClient,
-#         auth_user: None,
-#         attribute: str,
-#         direction: SortingDirection,
-#         expected: list[dict[str, Any]],
-#         classifications: list[Classification],
-#     ) -> None:
-#         response = await client.get(
-#             app.url_path_for(self.ROUTE_NAME), params={"sort": f'["{attribute}", "{direction}"]'}
-#         )
-#
-#         assert response.status_code == HTTP_200_OK
-#
-#         data = response.json()
-#
-#         for i in range(0, len(data)):
-#             assert data[i]["id"] == expected[i]["id"]
-#             assert data[i]["name"] == expected[i]["name"]
-#             assert data[i]["form"] == expected[i]["form"]
-#             assert data[i]["created_at"] is not None
-#             assert data[i]["updated_at"] is not None
-#
-#         assert response.headers.get("content-range") == "classification 0-49/10"
-#
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "attribute, direction, expected",
+        [
+            (
+                "id",
+                SortingDirection.ASC,
+                [
+                    {"name": "category: 0", "id": 1, "form": None},
+                    {"name": "category: 1", "id": 2, "form": None},
+                    {"name": "category: 2", "id": 3, "form": None},
+                    {"name": "category: 3", "id": 4, "form": None},
+                    {"name": "category: 4", "id": 5, "form": None},
+                    {"name": "category: 5", "id": 6, "form": None},
+                    {"name": "category: 6", "id": 7, "form": None},
+                    {"name": "category: 7", "id": 8, "form": None},
+                    {"name": "category: 8", "id": 9, "form": None},
+                    {"name": "category: 9", "id": 10, "form": None},
+                ],
+            ),
+            (
+                "id",
+                SortingDirection.DESC,
+                [
+                    {"name": "category: 9", "id": 10, "form": None},
+                    {"name": "category: 8", "id": 9, "form": None},
+                    {"name": "category: 7", "id": 8, "form": None},
+                    {"name": "category: 6", "id": 7, "form": None},
+                    {"name": "category: 5", "id": 6, "form": None},
+                    {"name": "category: 4", "id": 5, "form": None},
+                    {"name": "category: 3", "id": 4, "form": None},
+                    {"name": "category: 2", "id": 3, "form": None},
+                    {"name": "category: 1", "id": 2, "form": None},
+                    {"name": "category: 0", "id": 1, "form": None},
+                ],
+            ),
+            (
+                "name",
+                SortingDirection.ASC,
+                [
+                    {"name": "category: 0", "id": 1, "form": None},
+                    {"name": "category: 1", "id": 2, "form": None},
+                    {"name": "category: 2", "id": 3, "form": None},
+                    {"name": "category: 3", "id": 4, "form": None},
+                    {"name": "category: 4", "id": 5, "form": None},
+                    {"name": "category: 5", "id": 6, "form": None},
+                    {"name": "category: 6", "id": 7, "form": None},
+                    {"name": "category: 7", "id": 8, "form": None},
+                    {"name": "category: 8", "id": 9, "form": None},
+                    {"name": "category: 9", "id": 10, "form": None},
+                ],
+            ),
+            (
+                "name",
+                SortingDirection.DESC,
+                [
+                    {"name": "category: 9", "id": 10, "form": None},
+                    {"name": "category: 8", "id": 9, "form": None},
+                    {"name": "category: 7", "id": 8, "form": None},
+                    {"name": "category: 6", "id": 7, "form": None},
+                    {"name": "category: 5", "id": 6, "form": None},
+                    {"name": "category: 4", "id": 5, "form": None},
+                    {"name": "category: 3", "id": 4, "form": None},
+                    {"name": "category: 2", "id": 3, "form": None},
+                    {"name": "category: 1", "id": 2, "form": None},
+                    {"name": "category: 0", "id": 1, "form": None},
+                ],
+            ),
+        ],
+    )
+    async def test_list_classifications_sorted(
+        self,
+        app: FastAPI,
+        client: AsyncClient,
+        auth_user: None,
+        attribute: str,
+        direction: SortingDirection,
+        expected: list[dict[str, Any]],
+        classifications: list[Classification],
+    ) -> None:
+        response = await client.get(
+            app.url_path_for(self.ROUTE_NAME), params={"sort": f'["{attribute}", "{direction}"]'}
+        )
+
+        assert response.status_code == HTTP_200_OK
+
+        data = response.json()
+
+        for i in range(0, len(data)):
+            assert data[i]["id"] == expected[i]["id"]
+            assert data[i]["name"] == expected[i]["name"]
+            assert data[i]["form"] == expected[i]["form"]
+            assert data[i]["created_at"] is not None
+            assert data[i]["updated_at"] is not None
+
+        assert response.headers.get("content-range") == "classification 0-49/10"
+
 #     @pytest.mark.asyncio
 #     @pytest.mark.parametrize(
 #         "limit, offset, attribute, direction, expected",
