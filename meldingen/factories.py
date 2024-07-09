50d101b0d73a9850da38458df7c347e672c4a5e0
Youri Westerman <y.westerman@amsterdam.nl>
2024-07-09 10:22:48 +0200

    Specify melding type in factory

--- meldingen/factories.py
+++ meldingen/factories.py
@@ -3,6 +3,6 @@ from meldingen_core.factories import BaseAttachmentFactory
 from meldingen.models import Attachment, Melding
 
 
-class AttachmentFactory(BaseAttachmentFactory[Attachment]):
+class AttachmentFactory(BaseAttachmentFactory[Attachment, Melding]):
     def __call__(self, original_filename: str, melding: Melding) -> Attachment:
         return Attachment(original_filename, melding)
