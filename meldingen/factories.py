b798150c58d1d4fe2b8e70a4659858330a048979
Youri Westerman <y.westerman@amsterdam.nl>
2024-07-09 09:52:10 +0200

    Add attachment factory implementation

---
+++ meldingen/factories.py
@@ -0,0 +1,8 @@
+from meldingen_core.factories import BaseAttachmentFactory
+
+from meldingen.models import Attachment, Melding
+
+
+class AttachmentFactory(BaseAttachmentFactory[Attachment]):
+    def __call__(self, original_filename: str, melding: Melding) -> Attachment:
+        return Attachment(original_filename, melding)
