a857858bb8432aedd1ad551f07923d2e0ec77e91
Youri Westerman <y.westerman@amsterdam.nl>
2024-08-08 08:58:19 +0200

    Remove unused code from database session manager class

--- meldingen/database.py
+++ meldingen/database.py
@@ -1,42 +1,19 @@
 import contextlib
 from typing import AsyncIterator
 
-from sqlalchemy.ext.asyncio import AsyncConnection, AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker
 
 
 class DatabaseSessionManager:
-    _engine: AsyncEngine | None = None
-    _sessionmaker: async_sessionmaker[AsyncSession] | None = None
+    _engine: AsyncEngine
+    _sessionmaker: async_sessionmaker[AsyncSession]
 
     def __init__(self, engine: AsyncEngine):
         self._engine = engine
         self._sessionmaker = async_sessionmaker(autocommit=False, bind=self._engine, expire_on_commit=False)
 
-    async def close(self) -> None:
-        if self._engine is None:
-            raise Exception("DatabaseSessionManager is not initialized")
-        await self._engine.dispose()
-
-        self._engine = None
-        self._sessionmaker = None
-
-    @contextlib.asynccontextmanager
-    async def connect(self) -> AsyncIterator[AsyncConnection]:
-        if self._engine is None:
-            raise Exception("DatabaseSessionManager is not initialized")
-
-        async with self._engine.begin() as connection:
-            try:
-                yield connection
-            except Exception:
-                await connection.rollback()
-                raise
-
     @contextlib.asynccontextmanager
     async def session(self) -> AsyncIterator[AsyncSession]:
-        if self._sessionmaker is None:
-            raise Exception("DatabaseSessionManager is not initialized")
-
         session = self._sessionmaker()
         try:
             yield session
