f8f0b9a3f869ee5122030363c38e82fdbf056d7f
Youri Westerman <y.westerman@amsterdam.nl>
2024-09-30 16:06:17 +0200

    Verify that we get a 404 response if the file data could not be found

--- tests/api/v1/endpoints/test_melding.py
+++ tests/api/v1/endpoints/test_melding.py
@@ -1155,3 +1155,21 @@ class TestMeldingDownloadAttachment:
         )
 
         assert response.status_code == HTTP_404_NOT_FOUND
+
+    @pytest.mark.anyio
+    @pytest.mark.parametrize(
+        ["melding_text", "melding_state", "melding_token"],
+        [("klacht over iets", MeldingStates.CLASSIFIED, "supersecuretoken")],
+        indirect=True,
+    )
+    async def test_download_attachment_file_not_found(
+        self, app: FastAPI, client: AsyncClient, attachment: Attachment
+    ) -> None:
+        melding = await attachment.awaitable_attrs.melding
+
+        response = await client.get(
+            app.url_path_for(self.ROUTE_NAME, melding_id=melding.id, attachment_id=attachment.id),
+            params={"token": "supersecuretoken"},
+        )
+
+        assert response.status_code == HTTP_404_NOT_FOUND
