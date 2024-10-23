484089c4d80f25603508168d695ce6ae0ca43567
Youri Westerman <y.westerman@amsterdam.nl>
2024-10-23 10:37:09 +0200

    Only run migrations and run python commands when starting fastapi

--- meldingen-entrypoint.sh
+++ meldingen-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -eux
 
-# Run Alembic migrations
-alembic upgrade head
+if [ "$1" = "fastapi" ]; then
+  # Run Alembic migrations
+  alembic upgrade head
 
-# Run Python commands (ignoring errors)
-python main.py users add user@example.com || true
-python main.py static-forms add-primary --title "Hoofd formulier" || true
+  # Run Python commands (ignoring errors)
+  python main.py users add user@example.com || true
+  python main.py static-forms add-primary --title "Hoofd formulier" || true
+fi
 
 exec "$@"
