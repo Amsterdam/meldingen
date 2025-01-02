e73c15a99aeea896d2bd175233b248429f6ac866
ramonavic <r.hofmeister@amsterdam.nl>
2025-01-02 11:14:46 +0100

    adds correct primary form

--- commands/static_forms.py
+++ commands/static_forms.py
@@ -29,7 +29,7 @@ async def async_create_static_forms() -> None:
             """
 
             if form_type in current_form_types:
-                print(f"[red]Error[/red] - The {form_type} form already exists")
+                print(f"[red]Warning[/red] - The {form_type} form already exists")
                 continue
 
             label = form_type.capitalize()
@@ -40,15 +40,18 @@ async def async_create_static_forms() -> None:
                 display=FormIoFormDisplayEnum.form,
             )
 
-            component = FormIoTextAreaComponent(
-                label=f"{label}",
-                description="",
-                key=f"{form_type}",
-                type=FormIoComponentTypeEnum.text_area,
-                input=True,
-                auto_expand=True,
-                max_char_count=255,
-            )
+            if form_type == StaticFormTypeEnum.primary:
+                component = await generate_primary_form_component()
+            else:
+                component = FormIoTextAreaComponent(
+                    label=f"{label}",
+                    description="",
+                    key=f"{form_type}",
+                    type=FormIoComponentTypeEnum.text_area,
+                    input=True,
+                    auto_expand=True,
+                    max_char_count=255,
+                )
 
             components = await form.awaitable_attrs.components
             components.append(component)
@@ -57,6 +60,19 @@ async def async_create_static_forms() -> None:
 
             print(f"[green]Success[/green] - The {form_type} form has been created")
 
+
+async def generate_primary_form_component() -> FormIoTextAreaComponent:
+    return FormIoTextAreaComponent(
+        label="Wat wilt u melden?",
+        description="Typ hieronder geen telefoonnummer en e-mailadres in. We vragen dit later in dit formulier.",
+        key="primary",
+        type=FormIoComponentTypeEnum.text_area,
+        input=True,
+        auto_expand=True,
+        max_char_count=1000,
+        jsonlogic=None,
+        required=False
+    )
 
 @app.command()
 def create() -> None:
