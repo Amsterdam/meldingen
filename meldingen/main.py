6be9c712db2791ee5b4b3ccf21357451bf6c6b92
Youri Westerman <y.westerman@amsterdam.nl>
2025-01-17 12:11:09 +0100

    Remove structlog related code

--- meldingen/main.py
+++ meldingen/main.py
@@ -1,7 +1,3 @@
-from importlib import metadata
-from typing import Awaitable, Callable
-
-import structlog
 from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI
 from opentelemetry import trace
@@ -13,14 +9,12 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from sqlalchemy.exc import IntegrityError
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
-from starlette.responses import JSONResponse, Response
+from starlette.responses import JSONResponse
 from starlette.status import HTTP_409_CONFLICT
 
 from meldingen.api.v1.api import api_router
 from meldingen.config import settings
-from meldingen.logging import setup_logging
 from meldingen.middleware import ContentSizeLimitMiddleware
-from meldingen.utils import get_version
 
 
 def get_application() -> FastAPI:
@@ -30,38 +24,6 @@ def get_application() -> FastAPI:
         prefix=settings.url_prefix,
     )
     application.include_router(api_router)
-
-    @application.middleware("http")
-    async def logging_middleware(request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
-        """Middleware to log HTTP requests and responses. This middleware will
-        add additional useful information to the log."""
-        # Clear previous context variables
-        structlog.contextvars.clear_contextvars()
-
-        # Bind useful contextvars
-        structlog.contextvars.bind_contextvars(
-            path=request.url.path,
-            method=request.method,
-            client_host=request.client.host if request.client else None,
-            meldingen_version=get_version(),
-            meldingen_core_version=metadata.version("meldingen-core"),
-        )
-
-        response = await call_next(request)
-
-        # Bind the status code of the response to the contextvars
-        structlog.contextvars.bind_contextvars(
-            status_code=response.status_code,
-        )
-
-        if 400 <= response.status_code < 500:
-            logger.warn("Client error")
-        elif response.status_code >= 500:
-            logger.error("Server error")
-        else:
-            logger.info("OK")
-
-        return response
 
     @application.exception_handler(IntegrityError)
     async def sql_alchemy_integrity_error_handler(request: Request, exc: IntegrityError) -> JSONResponse:
@@ -97,6 +59,3 @@ tracer_provider.add_span_processor(processor)
 trace.set_tracer_provider(tracer_provider)
 
 FastAPIInstrumentor.instrument_app(app)
-
-setup_logging()
-logger = structlog.get_logger()
