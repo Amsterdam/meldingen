e172afb1fdca367103f0bce679e4671e45e687b0
ramonavic <r.hofmeister@amsterdam.nl>
2025-03-18 14:57:27 +0100

    validation of iss and aud with correct http exceptions

--- meldingen/authentication.py
+++ meldingen/authentication.py
@@ -2,9 +2,16 @@ from typing import Annotated
 
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2AuthorizationCodeBearer
-from jwt import ExpiredSignatureError, PyJWKClient, PyJWT
+from jwt import (
+    ExpiredSignatureError,
+    InvalidAudienceError,
+    InvalidIssuerError,
+    MissingRequiredClaimError,
+    PyJWKClient,
+    PyJWT,
+)
 from sqlalchemy.exc import NoResultFound
-from starlette.status import HTTP_401_UNAUTHORIZED
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED
 
 from meldingen.config import settings
 from meldingen.dependencies import jwks_client, py_jwt, user_repository
@@ -22,6 +29,16 @@ class UnauthenticatedException(HTTPException):
         super().__init__(status_code=HTTP_401_UNAUTHORIZED, detail=detail)
 
 
+class InvalidRequestException(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(status_code=HTTP_400_BAD_REQUEST, detail="invalid_request")
+
+
+class InvalidTokenException(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(status_code=HTTP_401_UNAUTHORIZED, detail="invalid_token")
+
+
 async def authenticate_user(
     token: Annotated[str, Depends(oauth2_scheme)],
     jwks_client: Annotated[PyJWKClient, Depends(jwks_client)],
@@ -31,13 +48,20 @@ async def authenticate_user(
     signing_key = jwks_client.get_signing_key_from_jwt(token)
 
     try:
-        payload = py_jwt.decode(token, signing_key.key, algorithms=["RS256"], audience="account")
-    except ExpiredSignatureError:
-        raise UnauthenticatedException("Token has expired")
+        payload = py_jwt.decode(
+            token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience=settings.auth_audience,
+            issuer=settings.issuer_url,
+            require=["exp", "aud", "iss", "email"],
+        )
+    except (ExpiredSignatureError, InvalidIssuerError, InvalidAudienceError):
+        raise InvalidTokenException()
+    except MissingRequiredClaimError:
+        raise InvalidRequestException()
 
     email = payload.get("email")
-    if email is None:
-        raise UnauthenticatedException("Invalid token")
 
     try:
         return await user_repository.find_by_email(email)
