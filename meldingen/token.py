6de2f0a4b1ed4a92663f35359362442c6520510d
ramonavic <r.hofmeister@amsterdam.nl>
2025-04-01 14:08:17 +0200

    add invalidate token to submit action (#413)
    * add invalidate token to submit action
    
    * remove unused overrides

--- meldingen/token.py
+++ meldingen/token.py
@@ -1,8 +1,19 @@
 from secrets import token_urlsafe
+from typing import override
 
-from meldingen_core.token import BaseTokenGenerator
+from meldingen_core.statemachine import MeldingStates
+from meldingen_core.token import BaseTokenGenerator, BaseTokenInvalidator
+
+from meldingen.models import Melding
 
 
 class UrlSafeTokenGenerator(BaseTokenGenerator):
     async def __call__(self) -> str:
         return token_urlsafe()
+
+
+class TokenInvalidator(BaseTokenInvalidator[Melding]):
+
+    @property
+    def allowed_states(self) -> list[str]:
+        return [MeldingStates.SUBMITTED]
