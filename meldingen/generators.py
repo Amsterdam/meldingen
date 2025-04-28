c0448df6a0075607841b0c1c0c287f5e08b064cb
Youri Westerman <y.westerman@amsterdam.nl>
2025-04-28 15:40:42 +0200

    Add class that will generate the public ids

---
+++ meldingen/generators.py
@@ -0,0 +1,14 @@
+import secrets
+import string
+
+
+class PublicIdGenerator:
+    _alphabet: str
+    def __init__(self, alphabet: str | None = None):
+        self._alphabet = alphabet
+        if alphabet is None:
+            self._alphabet = string.ascii_uppercase + string.digits
+
+    def __call__(self, length: int = 6) -> str:
+        alphabet = string.ascii_uppercase + string.digits
+        return "".join(secrets.choice(alphabet) for _ in range(length))
