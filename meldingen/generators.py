43ae3681918e805e46194946e743762343cdc857
Youri Westerman <y.westerman@amsterdam.nl>
2025-04-29 09:55:12 +0200

    Satisfy mypy

--- meldingen/generators.py
+++ meldingen/generators.py
@@ -6,9 +6,10 @@ class PublicIdGenerator:
     _alphabet: str
 
     def __init__(self, alphabet: str | None = None):
-        self._alphabet = alphabet
         if alphabet is None:
             self._alphabet = string.ascii_uppercase + string.digits
+        else:
+            self._alphabet = alphabet
 
     def __call__(self, length: int = 6) -> str:
         return "".join(secrets.choice(self._alphabet) for _ in range(length))
