458a819e0b9ac2e15b712b90e98d9c7defac22ca
Youri Westerman <y.westerman@amsterdam.nl>
2025-06-18 16:34:48 +0200

    Add happy case unit test for `PDOKAddressResolver`

--- tests/test_address.py
+++ tests/test_address.py
@@ -2,9 +2,9 @@ from unittest.mock import AsyncMock, Mock
 
 import pytest
 from meldingen_core.address import BaseAddressResolver
+from pdok_api_client.api.locatieserver_api import LocatieserverApi
 from pdok_api_client.models.free200_response import Free200Response
 from pdok_api_client.models.response import Response
-from pdok_api_client.api.locatieserver_api import LocatieserverApi
 from pydantic_core import ValidationError
 
 from meldingen.address import (
@@ -85,3 +85,31 @@ class TestPDOKAddressResolver:
         resolved = await resolve(123.0, 456.0)
 
         assert resolved is None
+
+    @pytest.mark.anyio
+    async def test_address_can_be_resolved(self) -> None:
+        doc = {}
+        data = Mock(Free200Response)
+        data.response = Mock(Response)
+        data.response.num_found = 1
+        data.response.docs = [doc]
+
+        api = AsyncMock(LocatieserverApi)
+        api.reverse_geocoder.return_value = data
+
+        address = Address(
+            street="Straatweglaan",
+            house_number=12,
+            house_number_addition="A",
+            postal_code="1111AA",
+            city="Amsterdam",
+        )
+
+        transformer = Mock(PDOKAddressTransformer)
+        transformer.return_value = address
+
+        resolve = PDOKAddressResolver(api, transformer, {})
+
+        resolved = await resolve(123.0, 456.0)
+
+        assert resolved == address
