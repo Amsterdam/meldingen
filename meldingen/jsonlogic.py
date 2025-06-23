b215417ca18d315dfc8a4e2187e6b268fbdc204c
ramonavic <r.hofmeister@amsterdam.nl>
2025-06-23 13:50:45 +0200

    validate if result is string, then throw error message

--- meldingen/jsonlogic.py
+++ meldingen/jsonlogic.py
@@ -6,8 +6,8 @@ from json_logic import jsonLogic
 
 class JSONLogicValidationException(Exception):
 
-    def __init__(self, message: str, input: dict) -> None:
-        self.message = message
+    def __init__(self, msg: str, input: dict) -> None:
+        self.msg = msg
         self.input = input
 
 
@@ -15,20 +15,8 @@ class JSONLogicValidator:
     def __call__(self, tests: str, data: dict[str, Any]) -> None:
         validation = json.loads(tests)
         result = jsonLogic(validation, data)
+
+        if isinstance(result, str):
+            raise JSONLogicValidationException(msg=result, input=data)
         if not result:
-
-            # TODO how to do this pretty?
-            error_message = next(iter(validation.values()))[3]
-            if isinstance(error_message, str):
-                raise JSONLogicValidationException(message=error_message, input=data)
-
-            raise JSONLogicValidationException(message="Input is not valid", input=data)
-
-
-# {"and" : [
-#     {">=": [{ "var": "value.length" }, 3]},
-#      {"<=": [{ "var": "value.length" }, 100]}
-#   ],
-#   true,
-#   "above two under 100"
-#   }
+            raise JSONLogicValidationException(msg="Input is not valid", input=data)
