ee38f635a5c21548dade571534e32dd8a5ad7e30
Joey <joey.hendriks@amsterdam.nl>
2025-07-17 16:06:58 +0200

    fix test

--- tests/test_wfs.py
+++ tests/test_wfs.py
@@ -11,5 +11,5 @@ def test_get_url_from_parameters() -> None:
 
     assert (
         url
-        == "https://example.com?TYPENAMES=typename&SRSNAME=urn%3Aogc%3Adef%3Acrs%3AEPSG%3A%3A4326&OUTPUTFORMAT=application%2Fjson&SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&COUNT=1000&FILTERS=filtering"
+        == "https://example.com?TYPENAMES=typename&SRSNAME=urn%3Aogc%3Adef%3Acrs%3AEPSG%3A%3A4326&OUTPUTFORMAT=application%2Fjson&SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&COUNT=1000&FILTER=filtering"
     )
