834452fb00e3b432584006a90b8fa4fd71731107
Youri Westerman <y.westerman@amsterdam.nl>
2025-08-05 16:43:21 +0200

    Verify validation passes when the text is not too long

--- tests/api/v1/endpoints/test_melding.py
+++ tests/api/v1/endpoints/test_melding.py
@@ -131,6 +131,29 @@ class TestMeldingCreate:
         assert violation.get("msg") == "Meldingtekst moet 1000 tekens of minder zijn."
         assert violation.get("input") == {"text": text}
 
+    @pytest.mark.anyio
+    async def test_create_melding_with_valid_text(
+        self, app: FastAPI, client: AsyncClient, primary_form: StaticForm
+    ) -> None:
+        text = (
+            "On the other hand, we denounce with righteous indignation and dislike men who are so "
+            "beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, "
+            "that they cannot foresee the pain and trouble that are bound to ensue; and equal blame "
+            "belongs to those who fail in their duty through weakness of will, which is the same as "
+            "saying through shrinking from toil and pain. These cases are perfectly simple and easy "
+            "to distinguish. In a free hour, when our power of choice is untrammelled and when nothing "
+            "prevents our being able to do what we like best, every pleasure is to be welcomed and every "
+            "pain avoided. But in certain circumstances and owing to the claims of duty or the "
+            "obligations of business it will frequently occur that pleasures have to be repudiated and "
+            "annoyances accepted. The wise man therefore always holds in these matters to this principle "
+            "of selection: he rejects pleasures to secure other greater pleasures, or else he endures "
+            "pains to avoid worse pains."
+        )
+
+        response = await client.post(app.url_path_for(self.ROUTE_NAME_CREATE), json={"text": text})
+
+        assert response.status_code == HTTP_201_CREATED
+
 
 class TestMeldingList(BaseUnauthorizedTest, BasePaginationParamsTest, BaseSortParamsTest):
     ROUTE_NAME: Final[str] = "melding:list"
