15925f3d26a8afa7f023c5c2b1e992e00cec81d4
Joey <joey.hendriks@amsterdam.nl>
2025-08-06 14:33:05 +0200

    another test for generator

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,6 +1,17 @@
 from meldingen.generators import PublicIdGenerator
 
 
+def test_public_id_generator() -> None:
+    generator = PublicIdGenerator("a")
+
+    alphabet = generator()
+
+    assert generator._alphabet == "a"
+
+    assert len(alphabet) == 6
+    assert alphabet == "aaaaaa"
+
+
 def test_public_id_generator_no_arguments() -> None:
     generator = PublicIdGenerator()
 
