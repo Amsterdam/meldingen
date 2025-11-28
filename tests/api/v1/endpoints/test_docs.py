7856c9f9942132dc55f699bc4a01c789f51b3980
ramonavic <r.hofmeister@amsterdam.nl>
2025-11-28 14:16:59 +0100

    and return type

--- tests/api/v1/endpoints/test_docs.py
+++ tests/api/v1/endpoints/test_docs.py
@@ -7,7 +7,7 @@ from httpx import AsyncClient
 class TestScalarEndpoint:
     ROUTE_NAME: Final[str] = "docs:scalar"
 
-    async def test_scalar_endpoint_returns_html_with_title(self, app: FastAPI, client: AsyncClient):
+    async def test_scalar_endpoint_returns_html_with_title(self, app: FastAPI, client: AsyncClient) -> None:
         response = await client.get(app.url_path_for(self.ROUTE_NAME))
         assert response.status_code == 200
         assert "text/html" in response.headers["content-type"].lower()
