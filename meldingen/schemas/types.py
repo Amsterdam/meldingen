27f50c5e7e95ed31e64e3bcf4bbfc626bffa2a0e
ramonavic <r.hofmeister@amsterdam.nl>
2025-12-01 13:03:21 +0100

    create base answer model and inherit text answer from it

--- meldingen/schemas/types.py
+++ meldingen/schemas/types.py
@@ -1,9 +1,9 @@
-from typing import Annotated, Any
+from typing import Annotated, Any, TypeAlias
 
 from geojson_pydantic import Feature as GeoJsonPydanticFeature
 from geojson_pydantic import Point
 from meldingen_core.address import Address as BaseAddress
-from pydantic import BaseModel, Field, StringConstraints
+from pydantic import BaseModel, Field, StringConstraints, AfterValidator
 from pydantic.dataclasses import dataclass
 from pydantic_extra_types.phone_numbers import PhoneNumber as PydanticPhoneNumber
 
@@ -16,6 +16,13 @@ class GeoJson(GeoJsonPydanticFeature[Point, dict[str, Any] | BaseModel]): ...
 @dataclass
 class Address(BaseAddress):
     house_number: int = Field(gt=0)
+
+
+StrippedStr: TypeAlias = Annotated[str, AfterValidator(lambda val: val.strip())]
+"""String that automatically strips leading and trailing whitespace. May be empty."""
+
+NonEmptyStrippedStr: TypeAlias = Annotated[StrippedStr, StringConstraints(min_length=1)]
+"""String that's automatically strips leading and trailing whitespace. Must not be empty."""
 
 
 class PhoneNumber(PydanticPhoneNumber):
@@ -36,5 +43,5 @@ class PhoneNumber(PydanticPhoneNumber):
 
 class FormIOConditional(BaseModel):
     show: bool
-    when: Annotated[str, StringConstraints(min_length=1, strip_whitespace=True)]
+    when: NonEmptyStrippedStr
     eq: str | int | float | bool | None
