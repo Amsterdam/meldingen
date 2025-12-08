e1f295209c4ddde8a92d0350e2836bb3e133683a
Joey <joey.hendriks@amsterdam.nl>
2025-12-08 11:27:46 +0100

    create user when logging into idp and user does not exist

--- meldingen/authentication.py
+++ meldingen/authentication.py
@@ -45,8 +45,6 @@ async def authenticate_user(
     py_jwt: Annotated[PyJWT, Depends(py_jwt)],
     user_repository: Annotated[UserRepository, Depends(user_repository)],
 ) -> User:
-
-    print("TOKENNNNNNNN", token)
     signing_key = jwks_client.get_signing_key_from_jwt(token)
 
     try:
