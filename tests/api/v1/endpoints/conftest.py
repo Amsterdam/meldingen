fe14d4233ec4308a2a244fea68dee92e0fa70b8f
ramonavic <r.hofmeister@amsterdam.nl>
2025-12-09 17:38:57 +0100

    add tests for list answers and refactor

--- tests/api/v1/endpoints/conftest.py
+++ tests/api/v1/endpoints/conftest.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from os import pipe2
 from typing import Any
 from uuid import uuid4
 
@@ -14,7 +13,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from meldingen.authentication import authenticate_user
 from meldingen.models import (
-    Answer,
     AnswerTypeEnum,
     Asset,
     AssetType,
@@ -293,6 +291,66 @@ async def melding_with_time_answers(
 
 
 @pytest.fixture
+async def melding_with_different_answer_types(
+     db_session: AsyncSession, melding_with_classification: Melding, formio_time_component: FormIoTimeComponent, formio_text_area_component: FormIoTextAreaComponent
+) -> Melding:
+    classification = melding_with_classification.classification
+    form = Form(title="test_form", display=FormIoFormDisplayEnum.form, classification=classification)
+
+    panel1 = FormIoPanelComponent(
+        title="Panel 1",
+        label="Page 1",
+        key="page1",
+        input=False,
+        type=FormIoComponentTypeEnum.panel,
+        position=1,
+    )
+
+    form.components.append(panel1)
+
+    db_session.add(form)
+    await db_session.commit()
+
+    formio_text_area_component.parent = panel1
+
+    panel2 = FormIoPanelComponent(
+        title="Panel 2",
+        label="Page 2",
+        key="page2",
+        input=False,
+        type=FormIoComponentTypeEnum.panel,
+        position=2,
+    )
+
+    form_components = await form.awaitable_attrs.components
+    form_components.append(panel2)
+
+    formio_time_component.parent = panel2
+
+    text_answer = TextAnswer(
+        text="John Doe",
+        melding=melding_with_classification,
+        question=Question(text="What is your name?", form=form, component=formio_text_area_component),
+        type=AnswerTypeEnum.text,
+    )
+
+    db_session.add(text_answer)
+    await db_session.commit()
+
+    time_answer = TimeAnswer(
+        time="14:30",
+        melding=melding_with_classification,
+        question=Question(text="What time is it?", form=form, component=formio_time_component),
+        type=AnswerTypeEnum.time,
+    )
+
+    db_session.add(time_answer)
+    await db_session.commit()
+
+    return melding_with_classification
+
+
+@pytest.fixture
 async def melding_with_asset(
     db_session: AsyncSession, melding_with_classification_with_asset_type: Melding, asset: Asset
 ) -> Melding:
@@ -561,16 +619,20 @@ def is_required(request: FixtureRequest) -> bool:
 
 
 @pytest.fixture
-def conditional(request: FixtureRequest) -> dict[str, Any] | None:
+def jsonlogic(request: FixtureRequest) -> str | None:
     if hasattr(request, "param"):
-        return dict(request.param)
+        return str(request.param)
     else:
-        return {"show": True, "when": "A", "eq": "B"}
+        return None
 
 
 @pytest.fixture
-async def form(db_session: AsyncSession, form_title: str, conditional: dict[str, Any]) -> Form:
-    form = Form(title=form_title, display=FormIoFormDisplayEnum.form)
+def formio_text_area_component(
+    db_session: AsyncSession,
+    conditional: dict[str, Any],
+    is_required: bool,
+    jsonlogic: str | None,
+) -> FormIoTextAreaComponent:
 
     component = FormIoTextAreaComponent(
         label="Wat is uw klacht?",
@@ -581,27 +643,41 @@ async def form(db_session: AsyncSession, form_title: str, conditional: dict[str,
         auto_expand=True,
         max_char_count=255,
         conditional=conditional,
+        required=is_required,
+        jsonlogic=jsonlogic,
     )
+    db_session.add(component)
+
+    return component
+
+
+@pytest.fixture
+def conditional(request: FixtureRequest) -> dict[str, Any] | None:
+    if hasattr(request, "param"):
+        return dict(request.param)
+    else:
+        return {"show": True, "when": "A", "eq": "B"}
+
+
+@pytest.fixture
+async def form(
+    db_session: AsyncSession,
+    form_title: str,
+    formio_text_area_component: FormIoTextAreaComponent,
+) -> Form:
+    form = Form(title=form_title, display=FormIoFormDisplayEnum.form)
 
     components = await form.awaitable_attrs.components
-    components.append(component)
+    components.append(formio_text_area_component)
 
-    question = Question(text=component.description, form=form)
+    question = Question(text=formio_text_area_component.description, form=form)
 
-    component.question = question
+    formio_text_area_component.question = question
 
     db_session.add(form)
     await db_session.commit()
 
     return form
-
-
-@pytest.fixture
-def jsonlogic(request: FixtureRequest) -> str | None:
-    if hasattr(request, "param"):
-        return str(request.param)
-    else:
-        return None
 
 
 @pytest.fixture
@@ -668,13 +744,14 @@ async def form_with_date_component(
 
 
 @pytest.fixture
-def formio_time_component(db_session: AsyncSession) -> FormIoTimeComponent:
+def formio_time_component(db_session: AsyncSession, is_required: bool) -> FormIoTimeComponent:
     component = FormIoTimeComponent(
         label="Tijd",
         description="",
         key="tijd",
         type=FormIoComponentTypeEnum.time,
         input=True,
+        required=is_required,
     )
     db_session.add(component)
     return component
@@ -686,9 +763,12 @@ async def form_with_time_component(
     form_title: str,
     form_panel: FormIoPanelComponent,
     formio_time_component: FormIoTimeComponent,
+    melding_with_classification: Melding,
     conditional: dict[str, Any],
 ) -> Form:
-    form = Form(title=form_title, display=FormIoFormDisplayEnum.form)
+    form = Form(
+        title=form_title, display=FormIoFormDisplayEnum.form, classification=melding_with_classification.classification
+    )
 
     formio_time_component.conditional = conditional
 
@@ -710,7 +790,10 @@ async def form_with_time_component(
 
 @pytest.fixture
 async def form_with_classification(
-    db_session: AsyncSession, form_title: str, jsonlogic: str | None, is_required: bool, conditional: dict[str, Any]
+    db_session: AsyncSession,
+    form_title: str,
+    formio_text_area_component: FormIoTextAreaComponent,
+    conditional: dict[str, Any],
 ) -> Form:
     form = Form(title=form_title, display=FormIoFormDisplayEnum.form)
 
@@ -723,30 +806,17 @@ async def form_with_classification(
         conditional=conditional,
     )
 
-    component = FormIoTextAreaComponent(
-        label="Wat is uw klacht?",
-        description="",
-        key="wat-is-uw_klacht",
-        type=FormIoComponentTypeEnum.text_area,
-        input=True,
-        auto_expand=True,
-        max_char_count=255,
-        jsonlogic=jsonlogic,
-        required=is_required,
-        conditional=conditional,
-    )
-
     panel_components = await panel.awaitable_attrs.components
-    panel_components.append(component)
+    panel_components.append(formio_text_area_component)
 
     components = await form.awaitable_attrs.components
     components.append(panel)
 
-    question = Question(text=component.description, form=form)
+    question = Question(text=formio_text_area_component.description, form=form)
 
     db_session.add(question)
 
-    component.question = question
+    formio_text_area_component.question = question
 
     classification_name = "test_classification"
 
@@ -760,52 +830,6 @@ async def form_with_classification(
     form.questions.append(question)
     db_session.add(form)
 
-    await db_session.commit()
-
-    return form
-
-
-@pytest.fixture
-async def form_with_time_question_component(
-    db_session: AsyncSession,
-    form_title: str,
-    conditional: dict[str, Any],
-    melding_with_classification: Melding,
-) -> Form:
-    form = Form(
-        title=form_title, display=FormIoFormDisplayEnum.form, classification=melding_with_classification.classification
-    )
-
-    panel = FormIoPanelComponent(
-        title="Panel 1",
-        label="Page 1",
-        key="page1",
-        input=False,
-        type=FormIoComponentTypeEnum.panel,
-        conditional=conditional,
-    )
-
-    component = FormIoTimeComponent(
-        label="Wanneer speelde dit?",
-        description="Geef aan hoe laat dit gebeurde.",
-        key="wanneer",
-        type=FormIoComponentTypeEnum.time,
-        input=True,
-        conditional=None,
-    )
-
-    panel_components = await panel.awaitable_attrs.components
-    panel_components.append(component)
-
-    components = await form.awaitable_attrs.components
-    components.append(panel)
-
-    question = Question(text="Hoe laat was dit?", form=form, component=component)
-    db_session.add(question)
-
-    component.question = question
-
-    db_session.add(form)
     await db_session.commit()
 
     return form
