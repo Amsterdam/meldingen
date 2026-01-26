bac6e223536438738088b5b2bb88635d0a9d8c6c
ramonavic <r.hofmeister@amsterdam.nl>
2026-01-26 11:16:42 +0100

    remove pkce

--- meldingen/api/v1/endpoints/docs.py
+++ meldingen/api/v1/endpoints/docs.py
@@ -20,7 +20,7 @@ async def scalar_api_reference(request: Request) -> HTMLResponse:
                         "authorizationCode": {
                             "x-scalar-client-id": settings.auth_client_id,
                             "selectedScopes": settings.auth_scopes,
-                            "x-usePkce": "SHA-256",
+                            # "x-usePkce": "SHA-256",
                             "x-scalar-redirect-uri": f"{request.base_url}docs/oauth2-redirect",
                         }
                     }
