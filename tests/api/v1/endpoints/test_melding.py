d83fc758fbcf3a079668560541ce8616d4d1d072
ramonavic <r.hofmeister@amsterdam.nl>
2026-03-09 11:53:19 +0100

    make sure OpenAPI contract says there is a 422 return exception and improve tests

--- tests/api/v1/endpoints/test_melding.py
+++ tests/api/v1/endpoints/test_melding.py
@@ -2589,6 +2589,7 @@ class TestMeldingQuestionAnswer:
             ("ab:cd", r"String should match pattern '^(?:[01]\d|2[0-3]):[0-5]\d$'"),
             (1000, "Input should be a valid string"),
             (10.00, "Input should be a valid string"),
+            (None, "Input should be a valid string"),
         ],
     )
     async def test_create_time_answer_invalid(
@@ -2626,6 +2627,7 @@ class TestMeldingQuestionAnswer:
         detail = body.get("detail")
         assert len(detail) == 1
         assert detail[0].get("msg") == error_message
+        assert detail[0].get("type") == "string_type"
 
     @pytest.mark.anyio
     @pytest.mark.parametrize(
