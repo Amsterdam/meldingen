1f47eb15195874e2da0739178ef7a92180781969
joeyamst <joey.hendriks@amsterdam.nl>
2026-08-27 11:32:13 +0200

    strips metadata from uploaded files (#751)
    * start of stripping metadata
    
    * remove unneeded changes
    
    * dont delete image when stripping metadata fails

--- meldingen/dependencies.py
+++ meldingen/dependencies.py
@@ -137,10 +137,13 @@ from meldingen.factories import (
 )
 from meldingen.generators import PublicIdGenerator
 from meldingen.image import (
+    BaseMetadataStripper,
     ImageOptimizerTask,
     IMGProxyImageOptimizer,
     IMGProxyImageOptimizerUrlGenerator,
     IMGProxyImageProcessor,
+    IMGProxyMetadataStripper,
+    IMGProxyMetadataStripUrlGenerator,
     IMGProxySignatureGenerator,
     IMGProxyThumbnailGenerator,
     IMGProxyThumbnailUrlGenerator,
@@ -1060,6 +1063,22 @@ def thumbnail_generator_task(
     return ThumbnailGeneratorTask(thumbnail_generator, attachment_repository)
 
 
+def img_proxy_metadata_strip_url_generator(
+    signature_generator: Annotated[IMGProxySignatureGenerator, Depends(img_proxy_signature_generator)],
+) -> IMGProxyMetadataStripUrlGenerator:
+    return IMGProxyMetadataStripUrlGenerator(
+        signature_generator, settings.imgproxy_base_url, settings.imgproxy_metadata_strip_quality
+    )
+
+
+def metadata_stripper(
+    url_generator: Annotated[IMGProxyMetadataStripUrlGenerator, Depends(img_proxy_metadata_strip_url_generator)],
+    http_client: Annotated[AsyncClient, Depends(http_client)],
+    filesystem_factory: Annotated[BaseFilesystemFactory, Depends(filesystem_factory)],
+) -> BaseMetadataStripper:
+    return IMGProxyMetadataStripper(url_generator, http_client, filesystem_factory)
+
+
 def malware_scanner(
     container_client: Annotated[ContainerClient, Depends(azure_container_client)],
 ) -> BaseMalwareScanner:
@@ -1077,6 +1096,7 @@ def attachment_ingestor(
     background_task_manager: BackgroundTasks,
     optimizer_task: Annotated[ImageOptimizerTask, Depends(image_optimizer_task)],
     thumbnail_task: Annotated[ThumbnailGeneratorTask, Depends(thumbnail_generator_task)],
+    metadata_stripper: Annotated[BaseMetadataStripper, Depends(metadata_stripper)],
 ) -> Ingestor:
     return Ingestor(
         scanner,
@@ -1084,6 +1104,7 @@ def attachment_ingestor(
         background_task_manager,
         optimizer_task,
         thumbnail_task,
+        metadata_stripper,
         str(settings.attachment_storage_base_directory),
     )
 
