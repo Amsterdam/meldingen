e91fdde28b28a344fbfb6bc1d45dc7a4f2df833c
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-09-16 14:03:51 +0200

    Add overloading

--- tests/api/test_utils.py
+++ tests/api/test_utils.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import BinaryIO, Literal
+from typing import BinaryIO, Literal, overload
 
 import pytest
 from fastapi import HTTPException
@@ -48,10 +48,24 @@ def open_resource_file(filename: str) -> BinaryIO:
     return open(str(resource_path), "rb")
 
 
+@overload
+def read_file(filename: str) -> bytes: ...
+
+
+@overload
+def read_file(filename: str, read_mode: Literal["r"]) -> str: ...
+
+
+@overload
+def read_file(filename: str, read_mode: Literal["rb", "rb+"]) -> bytes: ...
+
+
 def read_file(filename: str, read_mode: Literal["r", "rb", "rb+"] = "rb") -> str | bytes:
     if read_mode in ("rb", "rb+"):
         with open(filename, read_mode) as f:
-            return f.read()
+            result_bytes: bytes = f.read()
+            return result_bytes
 
     with open(filename, read_mode, encoding="utf-8") as f:
-        return f.read()
+        result_str: str = f.read()
+        return result_str
