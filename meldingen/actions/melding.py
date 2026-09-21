5f158c7e1f5343e787615275bce756531f71d7c3
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2026-09-21 15:46:15 +0200

    Fix typing and implement melding_repository

--- meldingen/actions/melding.py
+++ meldingen/actions/melding.py
@@ -9,15 +9,14 @@ from meldingen_core.actions.melding import MeldingAnswerDeleteAction as BaseMeld
 from meldingen_core.actions.melding import MeldingDeleteAssetAction as BaseMeldingDeleteAssetAction
 from meldingen_core.actions.melding import MeldingListAction as BaseMeldingListAction
 from meldingen_core.actions.melding import MeldingReclassifyAction as BaseMeldingReclassifyAction
-from meldingen_core.actions.melding import MeldingRetrieveAction as BaseMeldingRetrieveAction
 from meldingen_core.actions.melding import MeldingSubmitAction as BaseMeldingSubmitAction
 from meldingen_core.actions.melding import MeldingSubmitActionMelder as BaseMeldingSubmitActionMelder
 from meldingen_core.address import BaseAddressEnricher
 from meldingen_core.exceptions import NotFoundException
 from meldingen_core.filters import MeldingListFilters
 from meldingen_core.repositories import BaseMeldingRepository
+from meldingen_core.repository_helpers import retrieve_or_raise_not_found
 from meldingen_core.statemachine import MeldingBackofficeStates, MeldingTransitions
-from meldingen_core.token import TokenVerifier
 from starlette.status import HTTP_422_UNPROCESSABLE_CONTENT
 
 from meldingen.location import MeldingLocationIngestor, WKBToPointShapeTransformer
@@ -53,17 +52,14 @@ class MeldingListAction(BaseMeldingListAction[Melding]):
             )
 
 
-class MeldingRetrieveAction(BaseMeldingRetrieveAction[Melding]): ...
+class MeldingRetrieveAction:
+    _melding_repository: BaseMeldingRepository
 
+    def __init__(self, melding_repository: BaseMeldingRepository):
+        self._melding_repository = melding_repository
 
-class MelderMeldingRetrieveAction:
-    _verify_token: TokenVerifier[Melding]
-
-    def __init__(self, token_verifier: TokenVerifier[Melding]):
-        self._verify_token = token_verifier
-
-    async def __call__(self, melding_id: int, token: str) -> Melding:
-        return await self._verify_token(melding_id, token)
+    async def __call__(self, melding_id: int) -> Melding:
+        return await retrieve_or_raise_not_found(self._melding_repository, melding_id, "Melding not found")
 
 
 class AddContactInfoToMeldingAction(BaseMeldingAddContactInfoAction[Melding]): ...
@@ -85,7 +81,7 @@ class MeldingSubmitActionMelder(BaseMeldingSubmitActionMelder[Melding]): ...
 
 
 class AddLocationToMeldingAction:
-    _verify_token: TokenVerifier[Melding]
+    _melding_repository: BaseMeldingRepository
     _ingest_location: MeldingLocationIngestor
     _background_task_manager: BackgroundTasks
     _add_address: BaseAddressEnricher[Melding, Address]
@@ -93,20 +89,20 @@ class AddLocationToMeldingAction:
 
     def __init__(
         self,
-        token_verifier: TokenVerifier[Melding],
+        melding_repository: BaseMeldingRepository,
         location_ingestor: MeldingLocationIngestor,
         background_task_manager: BackgroundTasks,
         address_enricher: BaseAddressEnricher[Melding, Address],
         wkb_to_point_shape_transformer: WKBToPointShapeTransformer,
     ) -> None:
-        self._verify_token = token_verifier
+        self._melding_repository = melding_repository
         self._ingest_location = location_ingestor
         self._background_task_manager = background_task_manager
         self._add_address = address_enricher
         self._wkb_to_point_shape = wkb_to_point_shape_transformer
 
-    async def __call__(self, melding_id: int, token: str, location: GeoJson) -> Melding:
-        melding = await self._verify_token(melding_id, token)
+    async def __call__(self, melding_id: int, location: GeoJson) -> Melding:
+        melding = await retrieve_or_raise_not_found(self._melding_repository, melding_id, "Melding not found")
         melding = await self._ingest_location(melding, location)
 
         assert melding.geo_location is not None
@@ -128,7 +124,7 @@ class MeldingReclassifyAction(BaseMeldingReclassifyAction[Melding, Classificatio
 
 class MeldingGetPossibleNextStatesAction:
     _state_machine: MeldingStateMachine
-    _melding_repository: BaseMeldingRepository[Melding]
+    _melding_repository: BaseMeldingRepository
 
     def __init__(self, state_machine: MeldingStateMachine, repository: BaseMeldingRepository[Melding]) -> None:
         self._state_machine = state_machine
